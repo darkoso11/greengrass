acc40ab275af30239468ed6a9c1c61eb7d4afd07
Oswald Garcia <darkxdoso@gmail.com>
2023-02-24 01:26:47 -0600

    añadiendo componentes

--- js/main.js
+++ js/main.js
@@ -11,17 +11,76 @@ var colors = {
   NavajoWhitegress: "#D6CDA4",
   PowderBluegress: "#A4D6CD",
 };
+var texts = {
+  description: `
+  <p>
+    Are you tired of spending hours cutting and maintaining your lawn?
+    Do you want a beautiful and green garden without the upkeep that
+    comes with a natural lawn? Then synthetic grass is the solution for
+    you!
+  </p>
+  <p>
+    Our high-quality synthetic grass is an aesthetic and practical
+    alternative for any outdoor space. It is resistant to UV rays, which
+    means it will remain green and lush throughout the year, regardless
+    of the climate. Additionally, synthetic grass does not require
+    watering, mowing, or fertilizers, making it an eco-friendly and
+    low-maintenance option. Forget about spending time and money on
+    garden maintenance and spend your time enjoying it with friends and
+    family.
+  </p>
+  <p>
+    Install synthetic grass and you will have a beautiful and green
+    garden without worries!
+  </p>
+  `,
+  galleryIntro: `
+  <p>
+    Este es el intro de la galería.
+  </p>
+  `,
+};
 (() => {
   document.body.innerHTML += `
   <!-- Scripts -->
   <!-- Components -->
     <iframe id="navbar-iframe" class="d-none" onload="getElement('navbar')" src="./components/navbar.html" frameborder="0"></iframe>
     <iframe id="footer-iframe" class="d-none" onload="getElement('footer')" src="./components/footer.html" frameborder="0"></iframe>
+    <iframe id="form-iframe" class="d-none" onload="getElement('form')" src="./components/form.html" frameborder="0"></iframe>
   `;
+  for (let key in texts) {
+    let docElement = document.getElementById(key + "-text-data");
+    if (!!docElement || texts[key] !== '') {
+      texts[key] = docElement.innerHTML;
+      getText(key);
+    }
+  }
 })();
 function getElement(element) {
-  document.getElementById(element + "-container").innerHTML = document
-    .getElementById(element + "-iframe")
-    .contentWindow.document.getElementById(element + "-component").innerHTML;
-  cssCreate();
+  let docElement = document.getElementById(element + "-container");
+  if (!!docElement) {
+    docElement.innerHTML = document
+      .getElementById(element + "-iframe")
+      .contentWindow.document.getElementById(element + "-component").innerHTML;
+    doCssCreate();
+  }
+}
+
+function getText(element) {
+  let docElements = document.getElementsByClassName(
+    element + "-text-container"
+  );
+  console.log(docElements);
+  if (!!docElements && docElements[0] && !!texts[element.toString()]) {
+    for (const docElement of docElements) {
+      docElement.innerHTML = texts[element.toString()];
+    }
+    doCssCreate();
+  }
+}
+
+function doCssCreate() {
+  setTimeout(() => {
+    cssCreate()
+  }, 100);
 }
