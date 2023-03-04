f569696ddbc6556581aa2280535405688073d388
unknown <socialmedia.gatm@gmail.com>
2023-03-03 23:59:14 -0600

    Revision de Michelle 1

--- contact.php
+++ contact.php
@@ -14,18 +14,20 @@ $reply = new PHPMailer(true);
 try { 
     // SUSTITUIR EN $_POST[" -- AQUI --"] EL NOMBRE (id) DEL INPUT DONDE SE INTRODUCEN LOS SIGUIENTES DATOS
     $nombre=$_POST["nombre"];
+    $apellido=$_POST["apellido"];
     $correo=$_POST["correo"];
     $telefono=$_POST["telefono"];
     $region=$_POST["region"];
+    $asunto=$_POST["asunto"];
     $mensaje=$_POST["mensaje"];
     
     $mail->isSMTP();                                     
     $mail->Host = 'mail.santiagosyntheticgrass.com';
     $mail->SMTPAuth = true;                             
     $mail->Username = 'contact@santiagosyntheticgrass.com';             
-    $mail->Password = 'flN56bPZL4#761YN';           
+    $mail->Password = '0so.santiago.123456789/*';           
     $mail->SMTPSecure = 'tls';                         
-    $mail->Port = 587; //PUERTO CORREO
+    $mail->Port = 587; // 587
     $mail->CharSet = 'UTF-8';
 
     $mail->setFrom('contact@santiagosyntheticgrass.com', 'Santiago Synthetic Grass');
@@ -34,19 +36,21 @@ try {
     $mail->addAddress('contact@santiagosyntheticgrass.com');
 
     // CON COPIA A...
-    //$mail->addCC('');
+    $mail->addCC('socialmedia.gatm@gmail.com');
 
     $mail->isHTML(true);    
     // TITULO CON EL QUE ENVIAMOS EL CORREO
-    $mail->Subject = /* $asunto */'';
+    $mail->Subject = $asunto;
     // CONTENIDO EN HTML
     $mail->Body    = "
         <div style='color:#000;'>
             <ul>
                 <li>$asunto</li>
                 <li>$nombre</li>
+                <li>$apellido</li>
                 <li>$correo</li>
                 <li>$telefono</li>
+                <li>$region</li>
                 <li>$mensaje</li>
             </ul>
         </div>
@@ -79,14 +83,14 @@ try {
     $reply->Host = 'mail.santiagosyntheticgrass.com';
     $reply->SMTPAuth = true;                             
     $reply->Username = 'contact@santiagosyntheticgrass.com';             
-    $reply->Password = 'flN56bPZL4#761YN';           
+    $reply->Password = '0so.santiago.123456789/*';           
     $reply->SMTPSecure = 'tls';                         
-    $reply->Port = 587; //PUERTO CORREO
+    $reply->Port = 587; //587
     $reply->CharSet = 'UTF-8';
 
     $reply->setFrom('contact@santiagosyntheticgrass.com', 'Santiago Synthetic Grass');
     $reply->addAddress($correo);
-    $reply->isHTML(true);    
+    $reply->isHTML(true);     
     $reply->Subject = 'Confirmación de envió de mensaje';
 
     $reply->Body    = "Gracias por ponerte en contacto, en breve nos comunicaremos contigo";
